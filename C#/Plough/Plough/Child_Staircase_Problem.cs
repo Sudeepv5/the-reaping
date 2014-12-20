abca8ed9dc38d63f5af2e865cd737a2757b64533
Sudeepv5 <sudeepv5@ccs.neu.edu>
2014-12-20 17:30:34 -0500

    C# rep commit - 12/20
    C# rep commit

---
+++ C#/Plough/Plough/Child_Staircase_Problem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plough
+{
+    class Child_Staircase_Problem
+    {
+        static void Main(string[] args)
+        {
+            Child_Staircase_Problem csp = new Child_Staircase_Problem();
+            Console.Out.WriteLine("Ans: " + csp.Solution(10));
+            Console.Read();
+        }
+        public double Solution(double n)
+        {
+            double result = 0.0;
+            if (n == 0.0 | n == 1.0)
+                return 1.0;
+            if (n == 2.0)
+                return 2.0;
+            if (n == 3.0)
+                return 4.0;
+            if(n>3)
+            {
+                result = Solution(n - 1) + Solution(n - 2) + Solution(n - 3); 
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ C#/Plough/Plough/Remove_Duplicate_From_LinkedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plough
+{
+    class Remove_Duplicate_From_LinkedList
+    {
+        static void Main(string[] args)
+        {
+            Remove_Duplicate_From_LinkedList rdfl = new Remove_Duplicate_From_LinkedList();
+
+            LinkedList<int> llt_in = new LinkedList<int>();
+            llt_in.AddFirst(new LinkedListNode<int>(1));
+            llt_in.AddLast(new LinkedListNode<int>(1));
+            llt_in.AddLast(new LinkedListNode<int>(3));
+            llt_in.AddLast(new LinkedListNode<int>(3));
+            llt_in.AddLast(new LinkedListNode<int>(1));
+            llt_in.AddLast(new LinkedListNode<int>(5));
+            llt_in.AddLast(new LinkedListNode<int>(2));
+            llt_in.AddLast(new LinkedListNode<int>(5));
+            llt_in.AddLast(new LinkedListNode<int>(4));
+
+            var i = rdfl.Solution(llt_in).First;
+            while(i!=null)
+            {
+            Console.Out.WriteLine(i.Value);
+            i = i.Next;
+            }
+            Console.Read();
+        }
+
+        public LinkedList<int> Solution(LinkedList<int> llt)
+        {         
+            LinkedListNode<int> i, j;
+            i = llt.First;
+            while (i != null)
+            {
+                j = i.Next;
+                while (j != null)
+                {
+                    if (i.Value == j.Value)
+                    {
+                        var t = j.Next;
+                        llt.Remove(j);
+                        j = t;
+                    }
+                    else
+                        j = j.Next;
+                }
+                i = i.Next;
+            }
+
+            return llt;
+        }
+    }
+}
